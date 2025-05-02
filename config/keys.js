d261d663be81a410d032818293310ea3782e9650
MarekS16 <stremio@centrum.sk>
2025-05-02 11:31:27 +0200

    Delete config/keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-// Výchozí konfigurační klíče pro nasazení
-module.exports = {
-    tmdbApiKey: process.env.TMDB_API_KEY || ''  // Pro produkci používáme proměnnou prostředí nebo prázdný řetězec jako fallback
-};
